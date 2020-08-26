b47c0df5020dc817241201adff0646d85353ff81
FlorianSdorra <fsd114@posteo.de>
2020-08-26 16:35:58 +0200

    preparing

---
+++ src/components/CozyCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cozy-card.scss'
+
+const CozyCard = () => {
+    return(
+        <div className="cozy-card">
+            Hello
+        </div>
+    )
+}
+
+export default CozyCard;
