f471153bbf8ca96035409ba000619c4776be411c
FlorianSdorra <fsd114@posteo.de>
2020-08-26 18:11:04 +0200

    aligned 3 columns | details now

--- src/components/CozyCard.js
+++ src/components/CozyCard.js
@@ -4,7 +4,36 @@ import './cozy-card.scss'
 const CozyCard = () => {
     return(
         <div className="cozy-card">
-            Hello
+            <div className="top">
+                <div className="tile">
+                    <p>Ritual Energy Care</p>
+                </div>
+                <div className="tile">
+                    <p>Meditation</p>
+                </div>
+            </div>
+            <div className="middle">
+                <div className="egg"></div>
+                <div className="corners">
+                    <div>
+                        <p>Test</p>
+                    </div>
+                    <div>
+                        <p>Test</p>
+                    </div>
+                    <div>
+                        <p>Test</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="tile">
+                    <p>Affirmation</p>
+                </div>
+                <div className="tile">
+                    <p>Luxury Things</p>
+                </div>
+            </div>
         </div>
     )
 }
